8b58af324ed25f54cebb58a98086668e7f9e25c3
Ben Cameron <ben.cameron@d3tech.net>
2016-09-16 11:52:41 -0500

    Added ngModule for Angular 2 release

--- angular2-c360.ts
+++ angular2-c360.ts
@@ -1,6 +1,15 @@
-export * from './src/c360-context.service';
-export * from './src/c360-viewer.component';
-export * from './src/DefaultModelAdapter';
+import { NgModule } from '@angular/core';
+import { C360ContextService } from './src/c360-context.service';
+import { C360ViewerComponent } from './src/c360-viewer.component';
+
+@NgModule({
+  declarations: [ C360ViewerComponent ],
+  exports:      [ C360ViewerComponent ],
+  providers:    [ C360ContextService ]
+})
+export class AngularC360Module { }
+
+export { C360ContextService };
 export * from './src/IModelAdapter';
 export * from './src/UIAction';
 export * from './src/UIMessage';
